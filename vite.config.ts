b16b996e2533a0b6a12c2518573bc18b1ec0bd67
Ning Ren <renning22@gmail.com>
2023-11-30 23:00:37 -0800

    hack

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ai-town',
+  base: '/',
   plugins: [react()],
+  server: {
+    host: true,
+  }
 });
